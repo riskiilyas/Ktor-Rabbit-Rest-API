9f8a02a6ab4c06d6a6eab5ca5b08685a40c5a567
riskiilyas <riskiilyas03@gmail.com>
2022-05-01 09:50:52 +0700

    Working on Rabbit Breeds Route

--- src/main/kotlin/com/riskiilyas/models/RabbitBreeds.kt
+++ src/main/kotlin/com/riskiilyas/models/RabbitBreeds.kt
@@ -1,5 +1,8 @@
 package com.riskiilyas.models
 
+import com.riskiilyas.BASE_URL
+
+@kotlinx.serialization.Serializable
 data class RabbitBreeds(
     val breed_name: String,
     val img_url: String,
@@ -7,6 +10,14 @@ data class RabbitBreeds(
     val origin: String
 ) {
     companion object {
+        private val rabbitBreeds = listOf(
+            RabbitBreeds("Alaska", "$BASE_URL/rabbit_breeds/rabbit_alaska.jpg", 3.7f, "Europe"),
+            RabbitBreeds("Chinchilla", "$BASE_URL/rabbit_breeds/Chinchilla.jpg", 3.2f, "France"),
+            RabbitBreeds("Himalayan", "$BASE_URL/rabbit_breeds/Himalayan.jpg", 3.1f, "China"),
+            RabbitBreeds("Tan", "$BASE_URL/rabbit_breeds/Tan.jpg", 2.3f, "England"),
+            RabbitBreeds("Lionhead", "$BASE_URL/rabbit_breeds/Lionhead.jpg", 1.5f, "Belgium")
+        )
 
+        fun randomRabbitBreeds() = rabbitBreeds.random()
     }
 }
